ad4483ed22fbd25b98fff3a3f053ae1b84b20791
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-09-25 15:15:02 +0700

    updated libraries

--- research/duckdb-research/main.py
+++ research/duckdb-research/main.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List
 import os
 from datetime import timedelta
 import json
@@ -10,11 +11,13 @@ from helpers import (
     duckdb_describe_query,
     duckdb_getting_ids,
     duckdb_init_psql,
+    duckdb_upload_parquet_to_bucket,
     DuckDBToBigQueryMapper,
 )
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 load_dotenv("../../.env.shared")
 
@@ -28,6 +31,7 @@ etl_date = date.strftime("%Y-%m-%d")
 ### table config
 psql_schema = "public"
 psql_table = "b2x_checkout_transaction"
+
 
 ### begin ingestion
 with open("main_query.sql") as main_query:
@@ -52,43 +56,54 @@ with open("main_query.sql") as main_query:
         psql_schema=psql_schema, psql_table=psql_table
     )
 
-    logging.info("executing main query...")
-    main_df = db_conn.sql(
-        query=query_data,
-        params={
-            "min_id": indexes_df["min_id"].iloc[0],
-            "max_id": indexes_df["max_id"].iloc[0],
-            "psql_dstart": psql_dstart,
-            "psql_dend": psql_dend,
-        },
-    ).df()
-    logging.info("query executed.")
+    def main_job() -> None:
+        logging.info("executing main query...")
+        main_df = db_conn.sql(
+            query=query_data,
+            params={
+                "min_id": indexes_df["min_id"].iloc[0],
+                "max_id": indexes_df["max_id"].iloc[0],
+                "psql_dstart": psql_dstart,
+                "psql_dend": psql_dend,
+            },
+        ).df()
+        logging.info("query executed.")
 
-    logging.info(f"Retrieved {len(main_df)} rows with {len(main_df.columns)} columns.")
-    logging.info(
-        f"DataFrame memory usage: {main_df.memory_usage(deep=True).sum() / 1024**2:.2f} MB"
-    )
+        logging.info(
+            f"Retrieved {len(main_df)} rows with {len(main_df.columns)} columns."
+        )
+        logging.info(
+            f"DataFrame memory usage: {main_df.memory_usage(deep=True).sum() / 1024**2:.2f} MB"
+        )
 
-    # print(query_data)
+        # print(query_data)
 
-    logging.info("data preview:")
-    print(main_df.head())
+        logging.info("data preview:")
+        print(main_df.head())
+        return main_df
 
-    logging.info("running DESCRIBE to query...")
-    describe_df = duckdb_describe_query(
-        duck_conn=db_conn,
-        query=query_data,
-        psql_dstart=psql_dstart,
-        psql_dend=psql_dend,
-        indexes_df=indexes_df,
-    )
-    # print(describe_df)
+    def generate_schema_job() -> List:
+        logging.info("running DESCRIBE to query...")
+        describe_df = duckdb_describe_query(
+            duck_conn=db_conn,
+            query=query_data,
+            psql_dstart=psql_dstart,
+            psql_dend=psql_dend,
+            indexes_df=indexes_df,
+        )
+        print(describe_df)
 
-    logging.info("generating BQ external table schema...")
-    bq_schema_mapper = DuckDBToBigQueryMapper()
-    bq_schema = bq_schema_mapper.duckdb_describe_to_bq_schema(describe_df)
-    # print(json.dumps(bq_schema, indent=2))
+        logging.info("generating BQ external table schema...")
+        bq_schema_mapper = DuckDBToBigQueryMapper()
+        bq_schema = bq_schema_mapper.duckdb_describe_to_bq_schema(describe_df)
 
+        print(json.dumps(bq_schema, indent=2))
+        return bq_schema
+
+    def upload_parquet() -> None:
+        gcs_bucket = os.getenv("GCS_BUCKET")
+        duckdb_upload_parquet_to_bucket(duck_conn=db_conn, query=query_data)
+        gcs_bucket_path = f"gs://{gcs_bucket}/{psql_table}/dt={etl_date}/duckdb.parquet"
 
 # logging.info("executing main query:")
 # logging.info(f"{query_data}")
